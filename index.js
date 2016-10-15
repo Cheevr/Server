beb23d2d57ec2332884d484ac1a2b012f35cb984
Ravi Gairola <mallox@pyxzl.net>
2016-10-14 21:56:43 -0700

    Now using config for paths & automatically reading routes

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
+var config = require('config');
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var stylus = require('stylus');
 
-var app = express();
+var app = module.exports = express();
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '../static/views'));
+app.set('views', path.join(__dirname, '../' + config.paths.views));
 
 // Stylus to CSS compiler on request
 app.use('/css', stylus.middleware({
-    src: 'static/styles',
-    dest: 'cache',
-    compress: process.env.ENV != 'development',
-    sourcemap: process.env.ENV == 'development'
+    src: config.paths.styles,
+    dest: config.paths.cache,
+    compress: config.tier == 'production',
+    sourcemap: process.env.ENV != 'production'
 }));
 
 // Static files server
-app.use('/css|/js', express.static(path.join(__dirname, '../cache')));
-app.use('/img', express.static(path.join(__dirname, '../static/img')));
-app.use('/js', express.static(path.join(__dirname, '../static/js')));
+app.use('/css|/js', express.static(path.join(__dirname, '../' + config.paths.cache)));
+app.use('/img', express.static(path.join(__dirname, '../' + config.paths.img)));
+app.use('/js', express.static(path.join(__dirname, '../'+ config.paths.js)));
 app.get('/', (req, res) => res.render('index'));
 
+// Load all routes
+let routeDir = path.join(__dirname, '..', config.paths.routes);
+let files = fs.readdirSync(routeDir);
+for (let file of files) {
+    let route = require(path.join(routeDir, file));
+    app.use(route.endpoint, route);
+}
+
 // Debug handler
-app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: app.get('env') == 'development' ? err : {}
-    });
+app.use((req, res) => {
+    res.status(404).end();
 });
 
 // Server Startup
-app.listen(8080);
-console.log('listening on port 8080');
+app.listen(config.port);
+console.log('listening on port', config.port, 'with tier', config.tier);
 
