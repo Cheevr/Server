a72604ce8fb0b359463628efe8674c752445762b
Ravi Gairola <mallox@pyxzl.net>
2016-12-21 13:45:18 -0800

    Token API auth is only used when there's an auth config present

--- auth.js
+++ auth.js
@@ -3,11 +3,20 @@ const config = require('config');
 const tokens = require('express-token-api-middleware');
 
 
-// Make the default authentication type token
+/**
+ * Will add a configured authentication module that will add a user property to a request if the user authenticated.
+ * Any endpoint that wants to make use of will have to use app.auth with an automatic rejection handler or app.noauth
+ * with a handler that will parse users if it can, but still continue execution of the handler chain even if auth fails.
+ *
+ * Note that this will not include any endpoints to sign in/out but only the required methods to authenticate a request.
+ * @param app
+ */
 module.exports = app => {
-    app.auth = tokens(config.auth);
+    if (config.auth) {
+        let noAuthConfig = _.cloneDeep(config);
+        noAuthConfig.error = (req, res, next) => next();
 
-    let noAuthConfig = _.cloneDeep(config.auth);
-    noAuthConfig.error = (req, res, next) => next();
-    app.noauth = tokens(noAuthConfig);
+        app.auth = tokens(config.auth);
+        app.noauth = tokens(noAuthConfig);
+    }
 };
