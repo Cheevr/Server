06d919ccc61182b80e2a3601e54ff7e14904ea67
Ravi Gairola <mallox@pyxzl.net>
2017-01-18 14:27:32 -0800

    Added option to print (santized) version of configuration

--- config/backend.js
+++ config/backend.js
@@ -2,7 +2,8 @@ module.exports = {
     port: 8000,
     maxBodySize: '10mb',
     // Cache time in seconds for css, js and images
-    cacheTime: 86400000
+    cacheTime: 86400000,
+    printConfig: true
 };
 
 // Allowed domains for cross domain requests
--- settings.js
+++ settings.js
@@ -1,11 +1,41 @@
+const _ = require('lodash');
 const config = require('cheevr-config');
 const fs = require('fs');
+const Logger = require('cheevr-logging');
 const path = require('path');
+const util = require('util');
 
 
 const cwd = process.cwd();
 const viewDir = config.normalizePath(cwd, config.paths.views);
 viewDir.push(path.join(__dirname, 'static/views'));
+
+if (config.backend.printConfig) {
+    function clean(obj) {
+        for (let prop in obj) {
+            let val = obj[prop];
+            if (_.isObject(val) && !(val instanceof Buffer)){
+                clean(val);
+            } else if (_.isArray(val)) {
+                for (let entry of val) {
+                    clean(entry);
+                }
+            } else {
+                switch (prop) {
+                    case 'password':
+                    case 'salt':
+                    case 'secret':
+                    case 'key':
+                        obj[prop] = '*****'
+                }
+            }
+        }
+    }
+    let copy = _.cloneDeep(config);
+    delete copy._default;
+    clean(copy);
+    Logger.server.info('Launch Configuration: ' + util.format(copy));
+}
 
 module.exports = app => {
     // Configure the jsonp callback
