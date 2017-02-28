148378dca6ae13eff70be8fdc33dfa5af38a89a7
Ravi Gairola <mallox@pyxzl.net>
2017-02-27 23:24:39 -0800

    Updated user object to lazy load permissions and properties

---
+++ config/users.js
@@ -0,0 +1,6 @@
+module.exports = {
+    database: {
+        instance: '_default_',
+        index: 'accounts'
+    }
+};
--- users/user.js
+++ users/user.js
@@ -1,31 +1,56 @@
+let config = require('cheevr-config');
+let Database = require('cheevr-database');
+
 class User {
     /**
      *
      * @param {object} props
      */
     constructor(props) {
-        for (let prop of ['id', 'email', 'token', 'firstname', 'lastname', 'fullname']) {
-            Object.defineProperty(this, prop, { get: () => this['_' + prop] });
-        }
         for (let prop of props) {
             this['_' + prop] = props[prop];
+            Object.defineProperty(this, prop, {get: () => this['_' + prop]})
         }
+        this._db = Database.factory(config.users.database.instance);
     }
 
     /**
-     * Returns true or false depending on whether the user has the given permission.
-     * @param name
+     * Returns the permission value (likely true or false) the user has been given.
+     * @param {string} name The name of the permissions. Supports nested objects if the permission was configured that way
      */
-    permission(name) {}
+    async permission(name) {
+        if (this._permissions[name]) {
+            return this._permissions[name];
+        }
+        if (!this.id) {
+            throw new Error('Unable to retrieve user from database without user id');
+        }
+        let response = await this._db.get({
+            index: config.users.database.index,
+            type: 'permissions',
+            id: this.id
+        });
+        this._permissions = response._source;
+        return this._permissions[name];
+    }
 
     /**
-     * Loads the properties of the user from database.
-     * @private
+     * Loads any additional values that may be stored in the database.
      */
-    _load(prop) {
-        if (this['_' + prop]) {
-            return this['_' + prop];
+    async load() {
+        if (!this.id) {
+            throw new Error('Unable to retrieve user from database without user id');
         }
+        let response = await this._db.get({
+            index: config.users.database.index,
+            type: 'users',
+            id: this.id
+        });
+        for (let prop of response._source) {
+            this['_' + prop] = response._source[prop];
+            Object.defineProperty(this, prop, {get: () => this['_' + prop]})
+        }
+        return this;
     }
 }
 
